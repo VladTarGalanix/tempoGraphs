6a823664f074a1734641f0fbae6fd154dbc56e1f
floppyvlaDISK <tarasenkokansasvlad@gmail.com>
2017-12-06 23:34:57 +0200

    Exclude rule from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
         "prefer-template": 0,
         "prefer-arrow-callback": 0,
         "vars-on-top": 0,
-        "prefer-rest-params": 0
+        "prefer-rest-params": 0,
+        "prefer-spread": 0
     },
     "env": {        
         "browser": 1
